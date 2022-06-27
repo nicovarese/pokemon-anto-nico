22b8bcc30667a33ec7a8938fcf6c248dd6c0a80d
nico_varese <nicolasvaresee@gmail.com>
2022-06-27 16:02:35 -0300

    cambio 27/06 16:00

--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./Pokemon.css";
 
-function Pokemon(e) {
+function Pokemon({e}) {
+  const imagen = require(`../../Materiales/${e.nombre.toLowerCase()}.png`)
   return (
-    <p>{e.nombre}{e.numero}  <img src="" alt="" />  </p>
+  <div className='item'>
+      <p>{e.nombre}{e.numero}  <img className="foto-lista" src={imagen} alt="" />  </p>
+  </div>
   )
 }
 
--- src/Pages/lista-pokemon.jsx
+++ src/Pages/lista-pokemon.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Pokemon from '../Components/Pokemon/Pokemon'
 import pokeball from '../Materiales/Pokeball.png'
+import './lista-pokemon.css'
+import arrow from '../Materiales/Arrow.svg'
 
 function ListaPokemon({lista}) {
   return (
     <>
-    <div>
-        <img src={pokeball} alt="" />
-        <h1>Pokedex</h1>
-    </div>
-    <input type="search" />
+    <div className="contenedor">
+    <header>
+        <img className="pokeball-icono"src={pokeball} alt="" />
+        <h1>Pokédex</h1>
+        <div className="filtro">
+          <span>#</span>
+          <img className="flechita" src={arrow} alt="" />
+        </div>
+    </header>
+    <input type="search" placeholder='Buscar'/>
     {lista.map((e)=> <Pokemon e={e}/>)}
+    </div>
     </>   
   )
 }
